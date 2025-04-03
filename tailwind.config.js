2b96217cbeafbeda60ec7509e973da313e6013a5
Norman BELAID <Normanbelaid@gmail.com>
2025-04-03 16:00:17 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/**/*.{html,js}","*html"],
+  theme: {
+    extend: {
+      
+      fontFamily: {
+        texte: ['raleway'],
+        titre: [ 'Montserrat']
+      }
+    },
+  },
+  plugins: [],
+}
